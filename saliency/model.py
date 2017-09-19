d3e8a5454cd4891bd892c5ea6a7f4333ba478486
kskkwn <kskkwn@gmail.com>
2017-09-19 23:15:30 +0900

    saliency mnist

---
+++ saliency/model.py
@@ -0,0 +1,36 @@
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+
+
+if __name__ == '__main__':
+    lr = 0.01
+    momentum = 0.5
+
+    model = Net()
+    model.cuda()
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
